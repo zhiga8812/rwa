b2a4ec5dc44f595606c0ca47585d499674c0d1c1
zhiga8812 <zhiga88@icloud.com>
2026-03-04 23:31:18 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "register.html",
+  "thankyou.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
